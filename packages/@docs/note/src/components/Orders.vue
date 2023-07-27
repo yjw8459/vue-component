83a76ae6427df5896e2d3385946c347ad661acdb
yjw8459 <ryu01410@gmail.com>
2023-07-27 10:27:03 +0900

    feat: @docs/note Modify

--- packages/@docs/note/src/components/Orders.vue
+++ packages/@docs/note/src/components/Orders.vue
@@ -5,4 +5,6 @@
 <script setup lang="ts">
 import OrderButtonGroup from "./active/OrderButtonGroup.vue";
 import OrderList from "./table/OrderList.vue";
+const version: string = "0.0.1";
+console.log(version);
 </script>
