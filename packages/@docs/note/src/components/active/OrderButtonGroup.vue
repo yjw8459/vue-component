20ea5dbfab7e7b115c0a5c620ffd970f963eab26
yjw8459 <ryu01410@gmail.com>
2023-07-27 15:44:53 +0900

    feat: husky Test4

--- packages/@docs/note/src/components/active/OrderButtonGroup.vue
+++ packages/@docs/note/src/components/active/OrderButtonGroup.vue
@@ -3,7 +3,9 @@
   <v-btn>Button3</v-btn>
   <v-btn>Button3</v-btn>
   <v-btn>{{ btnTitle }}</v-btn>
+  <v-btn>{{ btnTitle2 }}</v-btn>
 </template>
 <script setup lang="ts">
 const btnTitle: string = "Button4";
+const btnTitle2: string = "Button5";
 </script>
