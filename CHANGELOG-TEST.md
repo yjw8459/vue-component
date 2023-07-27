7bdd5809193235ff05427bad04e7ca16c03b0abd
yjw8459 <ryu01410@gmail.com>
2023-07-27 15:44:00 +0900

    chore: husky Test 3

--- CHANGELOG-TEST.md
+++
@@ -1,5 +0,0 @@
-1. fix: Test
-2. docs
-3. style
-4. refactor
-5. build
--- packages/@docs/note/src/components/active/OrderButtonGroup.vue
+++ packages/@docs/note/src/components/active/OrderButtonGroup.vue
@@ -2,5 +2,8 @@
   <v-btn>Button3</v-btn>
   <v-btn>Button3</v-btn>
   <v-btn>Button3</v-btn>
+  <v-btn>{{ btnTitle }}</v-btn>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const btnTitle: string = "Button4";
+</script>
